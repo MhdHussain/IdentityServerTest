e86d67382aba9dee8591570ca048f35c138a3159
MhdHussein <mhdHussein@outlook.com>
2021-11-23 04:20:07 +0300

    Added Customers Controller

---
+++ BankOfDotNet.API/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace BankOfDotNet.API.Models
+{
+    public class Customer
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+    }
+}
